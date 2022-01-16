e1bab1ce5c1e9eb2486621939643fcf9b497a4d6
Joe Wills <joe.md.williams@gmail.com>
2022-01-16 15:28:25 +0000

    Add WeSplit Project

---
+++ Projects/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Joe Williams on 16/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
