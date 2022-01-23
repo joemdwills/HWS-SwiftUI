d77a3eaddd67f2834aeb49b69a239892a74fda63
Joe Wills <joe.md.williams@gmail.com>
2022-01-23 22:19:09 +0000

    Add day19 Challenge to repo

---
+++ Projects/day19/day19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  day19
+//
+//  Created by Joe Williams on 23/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Projects/day19/day19/day19App.swift
@@ -0,0 +1,17 @@
+//
+//  day19App.swift
+//  day19
+//
+//  Created by Joe Williams on 23/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct day19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
