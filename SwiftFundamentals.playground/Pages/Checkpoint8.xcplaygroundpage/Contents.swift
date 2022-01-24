7af81b6f27b51c32027539b22eef9fa7d7d2ed4d
Joe Wills <joe.md.williams@gmail.com>
2022-01-24 23:26:21 +0000

    day13 tutorial without checkpoint

---
+++ SwiftFundamentals.playground/Pages/Checkpoint8.xcplaygroundpage/Contents.swift
@@ -0,0 +1,122 @@
+import Foundation
+
+//Create a protocol, no function bodies allowed
+protocol Vehicle {
+    var name: String { get }
+    var currentPassengers: Int { get set }
+    func estimateTime(for distance: Int) -> Int
+    func travel(distance: Int)
+}
+
+struct Car: Vehicle {
+    let name = "Car"
+    var currentPassengers = 1
+    func estimateTime(for distance: Int) -> Int {
+        distance / 50
+    }
+    
+    func travel(distance: Int) {
+        print("I'm driving \(distance)km")
+    }
+    
+    func openSunroof() {
+        print("It's a nice day!")
+    }
+}
+
+struct Bicycle: Vehicle {
+    let name = "Bicycle"
+    var currentPassengers = 1
+    func estimateTime(for distance: Int) -> Int {
+        distance / 10
+    }
+    
+    func travel(distance: Int) {
+        print("I'm cycling \(distance)km")
+    }
+}
+
+func commute(distance: Int, using vehicle: Vehicle) {
+    if vehicle.estimateTime(for: distance) > 100 {
+        print("That's too slow! I'll try a different vehicle")
+    } else {
+        vehicle.travel(distance: distance)
+    }
+}
+
+func getTravelEstimates(using vehicles: [Vehicle], distance: Int) {
+    for vehicle in vehicles {
+        let estimate = vehicle.estimateTime(for: distance)
+        print("\(vehicle.name): \(estimate) hours to travel \(distance)km")
+    }
+}
+
+let car = Car()
+commute(distance: 100, using: car)
+let bike = Bicycle()
+commute(distance: 50, using: bike)
+
+getTravelEstimates(using: [car,bike], distance: 150)
+
+//Opaque Return types, use the code word 'some'
+//NOTE: The Equatable protocol is what allows us to use the == operator!
+
+func getRandomNumber() -> some Equatable {
+    Int.random(in: 1...6)
+}
+
+print(getRandomNumber() == getRandomNumber())
+
+//Extensions
+var quote = "    The truth is rarely pure and never simple     "
+
+extension String {
+    func trimmed() -> String {
+        self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    mutating func trim() {
+        self = self.trimmed()
+    }
+    
+    var lines: [String] {
+        self.components(separatedBy: .newlines)
+    }
+}
+
+//If you write a Custom Initialiser inside your struct, then Swift will get rid of the member-wise initialiser. If you want both, write your custom initialiser as an extension to your struct! For example:
+struct Book {
+    let title: String
+    let pageCount: Int
+    let readingHours: Int
+}
+
+extension Book {
+    init(title: String, pageCount: Int) {
+        self.title = title
+        self.pageCount = pageCount
+        self.readingHours = pageCount / 50
+    }
+}
+
+//You can set a protocol extension to provide a default value to a protocol value/func etc.
+
+protocol Person {
+    var name: String { get }
+    func sayHello()
+}
+
+extension Person {
+    func sayHello() {
+        print("Hi, I'm \(name)")
+    }
+}
+
+struct Employee: Person {
+    let name: String
+}
+
+let taylor = Employee(name: "Taylor Swift")
+taylor.sayHello()
+
+//Checkpoint8
