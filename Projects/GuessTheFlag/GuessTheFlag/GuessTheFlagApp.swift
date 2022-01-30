ea1ebc372ad8789f6328258cf629888adb651a9e
Joe Wills <joe.md.williams@gmail.com>
2022-01-30 19:02:02 +0000

    Create GuessTheFlag Project

---
+++ Projects/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Joe Williams on 30/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
