da4cfd48b8a2b6c86254f7c5631bdafb76dff0a7
Joe Wills <joe.md.williams@gmail.com>
2022-02-16 13:55:34 +0000

    Complete iExpense Project

--- Projects/iExpense/iExpense/ContentView.swift
+++ Projects/iExpense/iExpense/ContentView.swift
@@ -2,150 +2,48 @@
 //  ContentView.swift
 //  iExpense
 //
-//  Created by Joe Williams on 15/02/2022.
+//  Created by Joe Williams on 16/02/2022.
 //
 
 import SwiftUI
 
-class User: ObservableObject {
-    @Published var firstName = "Bilbo"
-    @Published var lastName = "Baggins"
-}
-
-struct ObjectView: View {
-    @StateObject var user = User()
-    
-    var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName).")
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
-        }
-    }
-}
-
-struct FirstView: View {
-    @State private var showSheet = false
-    
-    var body: some View {
-        Button("Show Sheet") {
-            // show the sheet
-            showSheet.toggle()
-        }
-        .sheet(isPresented: $showSheet) {
-            SecondView(name: "joemdwills")
-        }
-
-    }
-}
-
-struct SecondView: View {
-    var name: String
-    
-    @Environment(\.dismiss) var dismiss
-    
-    var body: some View {
-        Text("Hello, \(name)")
-        Button("Dismiss") {
-            dismiss()
-        }
-    }
-}
-
-struct ListView: View {
-    @State private var numbers = [Int]()
-    @State private var currentNumber = 1
+struct ContentView: View {
+    @StateObject var expenses = Expenses()
+    @State private var showingAddExpense = false
     
     var body: some View {
         NavigationView {
-            VStack {
-                List {
-                    ForEach(numbers, id: \.self) {
-                        Text("Row \($0)")
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "USD"))
                     }
-                    .onDelete(perform: removeRows)
                 }
-                Button("Add Numbers") {
-                    numbers.append(currentNumber)
-                    currentNumber += 1
-                }
+                .onDelete(perform: removeItems)
             }
-            .navigationTitle("onDelete()")
+            .navigationTitle("iExpense")
             .toolbar {
-                EditButton()
-            }
-        }
-    }
-    
-    func removeRows(at offsets: IndexSet) {
-        numbers.remove(atOffsets: offsets)
-    }
-}
-
-struct DefaultsView: View {
-    @AppStorage("tapCount") private var tapCount = 0
-    
-    var body: some View {
-        Button("Tap count: \(tapCount)") {
-            tapCount += 1
-        }
-    }
-}
-
-struct User2: Codable {
-    let firsName: String
-    let lastName: String
-}
-
-struct ArchivingView: View {
-    @State private var user = User2(firsName: "Taylor", lastName: "Swift")
-    
-    var body: some View {
-        
-        
-        Button("Save User") {
-            let encoder = JSONEncoder()
-            if let data = try? encoder.encode(user) {
-                UserDefaults.standard.set(data, forKey: "UserData")
-            }
-        }
-    }
-}
-
-struct ContentView: View {
-    @State private var viewsToShow = Views.list
-    
-    enum Views: String, CaseIterable {
-        case stateObject = "Observable Object"
-        case secondView = "Dismissable View"
-        case list = "Deletable Lists"
-        case tap = "App Storage"
-        case json = "JSON Encoder"
-    }
-    
-    var body: some View {
-        VStack {
-            Picker("Views", selection: $viewsToShow) {
-                ForEach(Views.allCases, id: \.self) {
-                    Text($0.rawValue)
+                Button {
+                    showingAddExpense = true
+                } label: {
+                    Image(systemName: "plus")
                 }
             }
-            .pickerStyle(.wheel)
-            Spacer()
-            switch viewsToShow {
-            case .stateObject:
-                ObjectView()
-            case .secondView:
-                FirstView()
-            case .list:
-                ListView()
-            case .tap:
-                DefaultsView()
-            case .json:
-                ArchivingView()
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
             }
-            Spacer()
+
         }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
---
+++ Projects/iExpense/iExpense/Expenses.swift
@@ -0,0 +1,29 @@
+//
+//  Expenses.swift
+//  iExpense
+//
+//  Created by Joe Williams on 16/02/2022.
+//
+
+import Foundation
+import UIKit
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        items = []
+    }
+}
