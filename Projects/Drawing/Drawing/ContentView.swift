5cd88f2e8617348c10c9fce35be5e200c649ea5a
Joe Wills <joe.md.williams@gmail.com>
2022-03-16 21:38:49 +0000

    Day 45

--- Projects/Drawing/Drawing/ContentView.swift
+++ Projects/Drawing/Drawing/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var amount = 0.0
+    
+    var blendMode: some View {
+        ZStack{
+            Image("Example")
+                .colorMultiply(.red)
+        }
+    }
+    
+    var blendMode2: some View {
+        VStack {
+            ZStack {
+                Circle()
+                    .fill(.red)
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(.green)
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(.blue)
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
+            }
+            .frame(width: 300, height: 300)
+            
+            Slider(value: $amount)
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
+        .ignoresSafeArea()
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        blendMode2
     }
 }
 
