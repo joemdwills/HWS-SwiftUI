6dd0b2a0c1ef21af654fd9ea65a9a132bcc99eb4
Joe Wills <joe.md.williams@gmail.com>
2022-03-21 17:10:01 +0000

    MVVM App Architecture

--- Projects/Edutainment/Edutainment/Model/Question.swift
+++ Projects/Edutainment/Edutainment/Model/Question.swift
@@ -10,16 +10,18 @@ import SwiftUI
 
 class Question: Identifiable, ObservableObject {
     let id: Int
-    let practice: Int
+    let practiceNumber: Int
     let correctAnswer: Int
     let multiplier: Int
     let text: String
+    let result: Bool
     
     init(id: Int, practice: Int) {
         self.id = id
-        self.practice = practice
+        self.practiceNumber = practice
         multiplier = Int.random(in: 2...12)
-        correctAnswer = self.practice * multiplier
-        text = "What is \(self.practice) X \(multiplier)?"
+        correctAnswer = self.practiceNumber * multiplier
+        text = "What is \(self.practiceNumber) X \(multiplier)?"
+        result = false
     }
 }
---
+++ Projects/Edutainment/Edutainment/ResultsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ResultsViewModel.swift
+//  Edutainment
+//
+//  Created by Joe Williams on 19/03/2022.
+//
+
+import Foundation
+
+class ResultsViewModel: ObservableObject {
+    
+}
