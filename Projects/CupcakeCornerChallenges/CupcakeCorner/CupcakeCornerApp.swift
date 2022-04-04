f4144e29f4ff73f2c6c35d1ebd4868623e9a06fb
Joe Williams <joe.md.williams@gmail.com>
2022-04-04 22:10:25 +0100

    Challenge1 Complete

---
+++ Projects/CupcakeCornerChallenges/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Joe on 02/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OrderView()
+        }
+    }
+}
