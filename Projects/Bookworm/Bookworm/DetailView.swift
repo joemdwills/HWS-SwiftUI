afa909c7944b54f244e1338d2a9e2351287b5349
Joe Williams <joe.md.williams@gmail.com>
2022-04-10 15:57:33 +0100

    Add DetailView

---
+++ Projects/Bookworm/Bookworm/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Joe on 10/04/2022.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
