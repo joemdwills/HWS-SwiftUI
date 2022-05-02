ffb1b6d381016905633c79de373b76a5d92494d7
Joe Williams <joe.md.williams@gmail.com>
2022-05-02 22:20:19 +0100

    Day 68 Complete

---
+++ Projects/BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Joe on 02/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FileManagerView()
+        }
+    }
+}
---
+++ Projects/BucketList/BucketList/EnumSwitchView.swift
@@ -0,0 +1,51 @@
+//
+//  EnumSwitchView.swift
+//  BucketList
+//
+//  Created by Joe on 02/05/2022.
+//
+
+import SwiftUI
+
+enum LoadingState {
+    case loading, success, failed
+}
+
+struct LoadingView: View {
+    var body: some View {
+        Text("Loading...")
+    }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("Success!")
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed.")
+    }
+}
+
+struct EnumSwitchView: View {
+    var loadingState = LoadingState.loading
+    
+    var body: some View {
+        switch loadingState {
+        case .loading:
+            LoadingView()
+        case .success:
+            SuccessView()
+        case .failed:
+            FailedView()
+        }
+    }
+}
+
+struct EnumSwitchView_Previews: PreviewProvider {
+    static var previews: some View {
+        EnumSwitchView()
+    }
+}
