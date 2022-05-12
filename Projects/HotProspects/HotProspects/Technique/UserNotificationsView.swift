3bde1b23242de2698852171c80127012d2b6b426
Joe Williams <joe.md.williams@gmail.com>
2022-05-12 22:02:59 +0100

    Day 82 Complete

---
+++ Projects/HotProspects/HotProspects/Technique/UserNotificationsView.swift
@@ -0,0 +1,44 @@
+//
+//  UserNotificationsView.swift
+//  HotProspects
+//
+//  Created by Joe on 12/05/2022.
+//
+
+import SwiftUI
+import UserNotifications
+
+struct UserNotificationsView: View {
+    var body: some View {
+        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set!")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+            
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the dogs"
+                content.subtitle = "They look hungry"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+            }
+        }
+    }
+}
+
+struct UserNotificationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserNotificationsView()
+    }
+}
